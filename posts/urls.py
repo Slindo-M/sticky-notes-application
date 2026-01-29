4d4273abba42aa0d8a1315667dd2e6e62408532a
Slindo-M <silindokuhle.majozi1315@gmail.com>
2026-01-29 17:19:08 +0200

    Delete posts directory

--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path("", views.sticky_note_display, name="sticky_note_display"),
-    path("sticky_note/new/", views.sticky_note_create, name="sticky_note_create"),
-    path("sticky_note/<int:pk>/edit/", views.sticky_note_update, name="sticky_note_update"),
-    path("sticky_note/<int:pk>/delete/", views.sticky_note_delete, name="sticky_note_delete"),
-]
